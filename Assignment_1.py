5484a3a3881f701f773dd3d123cdf52d62c65db5
satyampurdue-gif <satyampurdue@gmail.com>
2025-10-15 19:08:03 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+test_1 = num1 + num2
+test_2 = num1 - num2
+test_3 = num1 * num2
+test_4 = num1 / num2
+
+print ("Addtion :", test_1)
+print ("Subtraction :", test_2)
+print ("Multiplication:", test_3)
+print ("Division:", test_4)
